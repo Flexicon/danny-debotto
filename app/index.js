1be600d3983b101efdb01a2f4c2c29d9daa5fc51
Mike Repec <mikerepec@gmail.com>
2016-10-07 17:48:08 +0200

    (+) Feature Added - !randong, sounds to play

--- app/index.js
+++ app/index.js
@@ -12,21 +12,30 @@ var bot = new Discord.Client({
 var fs = require('fs');
 var support = null;
 fs.readFile('app/support.json', 'utf8', function (err, data) {
-  if (err) throw err;
-  support = JSON.parse(data);
-  console.log('json parsed');
+    if (err) throw err;
+    support = JSON.parse(data);
+    console.log('json parsed');
+});
+
+// Load sound filenames
+var sounds = null;
+fs.readdir('app/sounds/', function (err, files) {
+    if (err) throw err;
+    sounds = files;
+    console.log('sounds loaded');
 });
 
 // Event fires when bot is connected and ready to receive commands
 bot.on('ready', function(event) {
     console.log('Logged in as %s - %s\n', bot.username, bot.id);
-    bot.setPresence({game: { name: "!dongsupport" }});
+    bot.setPresence({game: { name: "!dongsupport, beta" }});
 });
 
 // Event fires when bot detects a new message he is able to read
 bot.on('message', function(user, userID, channelID, message, event) {
     if (message[0] === '!' && userID !== bot.id) {
 	switch(message) {
+	// !DONGSUPPORT
 	    case "!dongsupport":
 		var cmds = support.commands;
 		var msg = "The name's deBott, and I am an expert at delivering the following great commands: \n";
@@ -38,12 +47,47 @@ bot.on('message', function(user, userID, channelID, message, event) {
 		    message: msg
 		});
 		break;
+	// END !DONGSUPPORT
+	// --------------------------------------------------------------------------------------------
+	// !DONG
 	    case "!dong":
 		bot.sendMessage({
 		    to: channelID,
 		    message: "Oops I dropped my monster condoms, which I use for my MAGNUM DONG!"
 		});
 		break;
+	// END !DONG
+	// --------------------------------------------------------------------------------------------
+	// !RANDONG
+	    case "!randong":
+		var sound = sounds[Math.floor(Math.random() * sounds.length)];
+		var serverID = bot.channels[channelID].guild_id;
+		var voiceChannelID = bot.servers[serverID].members[userID].voice_channel_id;
+		bot.joinVoiceChannel(voiceChannelID, function(error, events) {
+		    if (error) return console.error(error);
+
+		    bot.getAudioContext(voiceChannelID, function (error, stream) {
+			if (error) return console.error(error);
+			fs.createReadStream('app/sounds/' + sound).pipe(stream, {end: false});
+
+			//The stream fires `done` when it's got nothing else to send to Discord.
+			stream.on('done', function () {
+			    bot.leaveVoiceChannel(voiceChannelID);
+			});
+		    });
+		});
+		break;
+	// END !RANDONG
+	// --------------------------------------------------------------------------------------------
+	// !LEAVE
+	    case "!leave":
+		var serverID = bot.channels[channelID].guild_id;
+		var voiceChannelID = bot.servers[serverID].members[userID].voice_channel_id;
+		bot.leaveVoiceChannel(voiceChannelID);
+		break;
+	// END !LEAVE
+	// --------------------------------------------------------------------------------------------
+	// !GOHOME
 	    case "!gohome":
 		bot.sendMessage({
 		    to: channelID,
@@ -51,6 +95,7 @@ bot.on('message', function(user, userID, channelID, message, event) {
 		});
 		bot.disconnect();
 		break;
+	// END !GOHOME
 	}
     }
 });
